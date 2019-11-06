5a4a06cebf8440fcd0f79770718dae915f99a375
Alice <aliceheine@hotmail.com>
2019-11-06 12:35:23 -0500

    form styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,12 @@
 // Global site tag (gtag.js) - Google Analytics 
 
-// (function($) {
-//   "use strict"; // Start of use strict
-
-//   console.log('thanks for checking out my site :)');
-
-//   // Smooth scrolling using jQuery easing
-//   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: (target.offset().top - 48)
-//         }, 1000, "easeInOutExpo");
-//         return false;
-//       }
-//     } 
-//   });
-
-// })(jQuery); // End of use strict
-
 
 var Utils = (function () {    
   function init(options) {
-    console.log('thanks for checking out my site ali :)');
+    console.log('thanks for checking out my site  :)');
   }
   return {
     init: init
   }
 
 })();
-
-$(document).ready(function(){
-  Utils.init();
-});
